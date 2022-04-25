1bf113da0bc539fc232e1e8e59199667ba95d64b
Serge Pillay <urza45@live.fr>
2022-04-25 11:05:54 +0200

    Add show list and detail request of a product

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -3,23 +3,41 @@
 namespace App\Controller;
 
 use App\Repository\ProductRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ApiController extends AbstractController
 {
     /**
      * Return list of product in a json response
      * 
-     * @Route("/api/products", name="api_product_list", methods="GET")
+     * @Route("/api/products", name="api_product_list", methods={"GET"})
      */
-    public function index(ProductRepository $repoProduct, SerializerInterface $serializer): Response
+    public function showProductsList(ProductRepository $repoProduct, SerializerInterface $serializer): Response
     {
         $products = $repoProduct->findAll();
 
         $data = $serializer->serialize($products, 'json');
+
+        $response = new Response($data);
+        $response->headers->set('Content-Type', 'application/json');
+
+        return $response;
+    }
+
+    /**
+     * Return list of product in a json response
+     * 
+     * @Route("/api/products/{index}", name="api_product", methods={"GET"}, requirements={"index"="\d+"})
+     */
+    public function showProduct(ProductRepository $repoProduct, SerializerInterface $serializer, Request $request): Response
+    {
+        $product = $repoProduct->findOneBy(['id' => $request->get('index')]);
+
+        $data = $serializer->serialize($product, 'json');
 
         $response = new Response($data);
         $response->headers->set('Content-Type', 'application/json');
